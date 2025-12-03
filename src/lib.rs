181860c12e6d3a44cbd0ab6607a4221152d05ac7
jsgroth <1137683+jsgroth@users.noreply.github.com>
2025-12-02 19:03:39 -0600

    solution timing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::fmt::Display;
+use std::time::Instant;
 use std::{env, fs, io};
 
 pub fn read_input() -> io::Result<String> {
@@ -9,12 +10,26 @@ pub fn read_input() -> io::Result<String> {
     fs::read_to_string(path)
 }
 
+fn time<T>(input: &str, f: impl Fn(&str) -> T) {
+    const RUNS: u128 = 10;
+
+    let mut total_nanos = 0;
+    for _ in 0..RUNS {
+        let start_time = Instant::now();
+        f(input);
+        total_nanos += (Instant::now() - start_time).as_nanos();
+    }
+
+    let total_millis = total_nanos / 1_000_000 / RUNS;
+    println!("{total_millis} ms");
+}
+
 pub fn run<T1, T2, F1, F2>(part1: F1, part2: F2) -> Result<(), Box<dyn Error>>
 where
     T1: Display,
     T2: Display,
-    F1: FnOnce(&str) -> T1,
-    F2: FnOnce(&str) -> T2,
+    F1: Fn(&str) -> T1,
+    F2: Fn(&str) -> T2,
 {
     let input = read_input()?;
 
@@ -23,6 +38,14 @@ where
 
     let solution2 = part2(&input);
     println!("{solution2}");
+
+    if env::var("AOCTIME").is_ok_and(|var| !var.is_empty()) {
+        print!("Part 1: ");
+        time(&input, part1);
+
+        print!("Part 2: ");
+        time(&input, part2);
+    }
 
     Ok(())
 }
